3e3672ecdc039b1d565ac2d007785ba7e411a0be
H9draIrawan <hendra.coding.952@gmail.com>
2026-09-25 15:35:11 +0700

    little modification navbar login/logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { MdFavorite } from "react-icons/md";
 import { NavLink } from "react-router-dom";
-
 function Navbar() {
 	const navItems = [
 		{
@@ -21,6 +20,8 @@ function Navbar() {
 			path: "/dashboard",
 		},
 	];
+
+	const isLoggedIn = false;
 
 	return (
 		<nav className="w-full border-b border-slate-200 bg-white px-4 py-3 shadow-sm sm:px-8">
@@ -44,10 +45,27 @@ function Navbar() {
 						))}
 					</ul>
 				</div>
-				<div className="flex flex-1 justify-end gap-8">
-					<MdFavorite className="text-4xl" />
-					<FaShoppingCart className="text-4xl" />
-					<FaUser className="text-4xl" />
+				<div className="flex flex-1 items-center justify-end gap-8">
+					{isLoggedIn ? (
+						<>
+							<MdFavorite className="text-4xl" />
+							<FaShoppingCart className="text-4xl" />
+							<FaUser className="text-4xl" />
+							<button
+								type="button"
+								className="rounded-full border border-slate-300 px-5 py-2 font-medium uppercase text-2xl text-white bg-red-500 transition hover:border-red-500 hover:bg-white hover:text-red-500"
+							>
+								Logout
+							</button>
+						</>
+					) : (
+						<NavLink
+							to="/login"
+							className="rounded-full px-5 py-2 font-medium uppercase text-2xl bg-blue-500 text-white transition border-2 hover:border-blue-500 hover:bg-white hover:text-blue-500"
+						>
+							Login
+						</NavLink>
+					)}
 				</div>
 			</div>
 		</nav>
